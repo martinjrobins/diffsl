b56f05e9649de359ac8b85d8201bcf5833217208
martinjrobins <martinjrobins@gmail.com>
2022-07-18 11:31:02 +0100

    stated with my old diff eq syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,38 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+#[derive(Parser)]
+#[grammar = "mmt_grammar.pest"] // relative to src
+struct MmtParser;
+
 #[cfg(test)]
 mod tests {
+    use crate::pest::Parser;
+    use crate::MmtParser;
+    use crate::Rule;
+    use std::fs;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let unparsed_file = fs::read_to_string("src/test.mmt").expect("cannot read file");
+        let list = MmtParser::parse(Rule::ident_list, &unparsed_file)
+            .expect("unsuccessful parse") // unwrap the parse result
+            .next()
+            .unwrap(); // get and unwrap the `file` rule; never fails
+
+        let mut record_count: u64 = 0;
+        for record in list.into_inner() {
+            match record.as_rule() {
+                Rule::ident => {
+                    record_count += 1;
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        println!("Number of records: {}", record_count);
     }
+
+
 }

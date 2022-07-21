d74f982d1957a3006601d08bd007c09fea5ddd0f
martinjrobins <martinjrobins@gmail.com>
2022-07-21 21:27:39 +0100

    got draft of pest grammer

--- src/lib.rs
+++ src/lib.rs
@@ -3,36 +3,28 @@ extern crate pest;
 extern crate pest_derive;
 
 #[derive(Parser)]
-#[grammar = "mmt_grammar.pest"] // relative to src
-struct MmtParser;
+#[grammar = "ms_grammar.pest"] // relative to src
+struct MsParser;
 
 #[cfg(test)]
 mod tests {
     use crate::pest::Parser;
-    use crate::MmtParser;
+    use crate::MsParser;
     use crate::Rule;
     use std::fs;
 
+    const MS_FILENAMES: &[&str] = &["test_circuit.ms", "test_fishers.ms", "test_pk.ms"];
+
+    const BASE_DIR: &str = "src";
+
     #[test]
-    fn it_works() {
-        let unparsed_file = fs::read_to_string("src/test.mmt").expect("cannot read file");
-        let list = MmtParser::parse(Rule::ident_list, &unparsed_file)
-            .expect("unsuccessful parse") // unwrap the parse result
-            .next()
-            .unwrap(); // get and unwrap the `file` rule; never fails
-
-        let mut record_count: u64 = 0;
-        for record in list.into_inner() {
-            match record.as_rule() {
-                Rule::ident => {
-                    record_count += 1;
-                }
-                _ => unreachable!(),
-            }
+    fn can_parse() {
+        for filename in MS_FILENAMES {
+            let unparsed_file =
+                fs::read_to_string(BASE_DIR.to_owned() + "/" + filename).expect("cannot read file");
+            let _list =
+                MsParser::parse(Rule::main, &unparsed_file).expect("unsuccessful parse");
+            // unwrap the parse result
         }
-
-        println!("Number of records: {}", record_count);
     }
-
-
 }

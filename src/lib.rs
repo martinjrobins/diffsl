21f2c3087798223c70bee4c422ce55a91062ba9f
martinjrobins <martinjrobins@gmail.com>
2022-08-15 21:24:57 +0100

    working out compiler errors for parser

--- src/lib.rs
+++ src/lib.rs
@@ -11,87 +11,273 @@ use pest::error::Error;
 use pest::iterators::Pair;
 
 #[derive(Debug)]
-pub enum Expr {
+pub enum Ast {
+    Model {
+        name: String,
+        unknowns: Vec<Ast>,
+        statements: Vec<Ast>,
+    },
+    Unknown {
+        name: String,
+        dependents: Vec<String>,
+        codomain: Option<Box<Ast>>,
+    },
+
+    Definition {
+        name: String,
+        rhs: Box<Ast>,
+    },
+
+    Submodel {
+        name: String,
+        local_name: String,
+        args: Vec<Ast>,
+    },
+
+    Equation {
+        lhs: Box<Ast>,
+        rhs: Box<Ast>,
+    },
+
+    RateEquation {
+        name: String,
+        rhs: Box<Ast>,
+    },
+
+    Range {
+        lower: f64,
+        upper: f64,
+    },
+
     Binop {
         op: char,
-        left: Box<Expr>,
-        right: Box<Expr>,
+        left: Box<Ast>,
+        right: Box<Ast>,
     },
 
     Monop {
         op: char,
-        child: Box<Expr>,
+        child: Box<Ast>,
     },
 
     Call {
         fn_name: String,
-        args: Vec<Expr>,
+        args: Vec<Ast>,
+    },
+
+    CallArg {
+        name: String,
+        expression: Box<Ast>,
     },
 
     Number(f64),
 
-    Variable(String),
+    Name(String),
 }
 
-#[derive(Debug)]
-pub enum EquationType {
-    Algebraic,
-    RateEqn { variable: Variable },
+#[derive(Clone, Copy)]
+struct Env<'a> {
+    models: Vec<&'a str>,
+    vars: Vec<&'a str>,
+    domains: Vec<&'a str>,
 }
 
-#[derive(Debug)]
-pub struct Equation {
-    kind: EquationType,
-    rhs: Box<Expr>,
+//sign       = @{ ("-"|"+")? }
+//factor_op  = @{ "*"|"/" }
+fn parse_sign(pair: Pair<Rule>) -> char {
+    *pair.into_inner().next().unwrap().as_str().chars().collect::<Vec<char>>().first().unwrap()
 }
 
-#[derive(Debug)]
-pub struct Domain {
-    name: String,
-    extents: (f64, f64),
+//name       = @{ 'a'..'z' ~ ("_" | 'a'..'z' | 'A'..'Z' | '0'..'9')* }
+//domain_name = @{ 'A'..'Z' ~ ('a'..'z' | 'A'..'Z' | '0'..'9')* }
+fn parse_name(pair: Pair<Rule>) -> String {
+    pair.into_inner().next().unwrap().as_str().to_string()
 }
 
-#[derive(Debug)]
-pub struct Variable {
-    name: String,
-    model: Model,
-    domain: Domain,
-    constant: bool,
-    state: bool,
-}
+fn parse_value<'a>(pair: Pair<Rule>, env: Env<'a>) -> Ast {
+    let mut inner = pair.into_inner();
+    let parse_value_env = |pair| parse_value(pair, env);
+    match pair.as_rule() {
+        // name       = @{ 'a'..'z' ~ ("_" | 'a'..'z' | 'A'..'Z' | '0'..'9')* }
+        // domain_name = @{ 'A'..'Z' ~ ('a'..'z' | 'A'..'Z' | '0'..'9')* }
+        Rule::name | Rule::domain_name => Ast::Name(inner.next().unwrap().as_str().to_string()),
 
-#[derive(Debug)]
-pub struct Model {
-    name: String,
-    variables: Vec<Variable>,
-    equations: Vec<Equation>,
-    submodels: Vec<Model>,
-}
+        // integer    = @{ ('0'..'9')+ }
+        // real       = @{ ( ('0'..'9')+ ~ "." ~ ('0'..'9')+ ) | integer }
+        Rule::integer | Rule::real => Ast::Name(inner.next().unwrap().as_str().parse().unwrap()),
 
-impl Model {
-    pub fn create(rule: Pair<Rule>) -> Model {
-        // "model" ~ name ~ "(" ~ unknown? ~ ("," ~ unknown)* ~ ")" ~ "{" ~ statement* ~ "}" 
-        let mut inner_rules = line.into_inner();
-        let name = inner_rules.next().unwrap().as_str();
-        let mut model = Model {
-            name: name,
-            variables: Vec::new(),
-            equations: Vec::new(),
-            submodels: Vec::new(),
-        };
+        // model = { "model" ~ name ~ "(" ~ unknown? ~ ("," ~ unknown)* ~ ")" ~ "{" ~ statement* ~ "}" }
+        Rule::model => {
+            let name = parse_name(inner.next().unwrap());
+            let unknowns = inner
+                .take_while(|pair| pair.as_rule() == Rule::unknown)
+                .map(parse_value_env)
+                .collect();
+            let statements = inner.map(parse_value_env).collect();
+            env.models.push(&name);
+            Ast::Model {
+                name,
+                unknowns,
+                statements,
+            }
+        }
+        // definition = { "let" ~ name ~ "=" ~ expression }
+        Rule::definition => {
+            let name = parse_name(inner.next().unwrap());
+            let rhs = Box::new(parse_value_env(inner.next().unwrap()));
+            env.vars.push(&name);
+            Ast::Definition { name, rhs }
+        }
+
+        // range      = { "[" ~ real ~ "..." ~ real ~ "]" }
+        Rule::range => Ast::Range {
+            lower: inner.next().unwrap().as_str().parse().unwrap(),
+            upper: inner.next().unwrap().as_str().parse().unwrap(),
+        },
+
+        // domain     = { range | domain_name }
+        Rule::domain => parse_value_env(inner.next().unwrap()),
+
+        // codomain   = { "->" ~ domain }
+        Rule::codomain => parse_value_env(inner.next().unwrap()),
+
+        //unknown    = { name ~ dependents? ~ codomain? }
+        Rule::unknown => {
+            let name = parse_name(inner.next().unwrap());
+            //dependents = { "(" ~ name ~ ("," ~ name )* ~ ")" }
+            let dependents = if inner.peek().unwrap().as_rule() == Rule::dependents {
+                inner
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .map(parse_name)
+                    .collect()
+            } else {
+                Vec::new()
+            };
+            let codomain = if inner.peek().is_some() {
+                Some(Box::new(parse_value_env(inner.next().unwrap())))
+            } else {
+                None
+            };
+            Ast::Unknown {
+                name,
+                dependents,
+                codomain,
+            }
+        }
+        //statement  = { definition | submodel | rate_equation | equation }
+        Rule::statement => parse_value_env(pair.into_inner().next().unwrap()),
+
+        // definition = { "let" ~ name ~ "=" ~ expression }
+        Rule::definition => Ast::Definition {
+            name: parse_name(inner.next().unwrap()),
+            rhs: Box::new(parse_value_env(inner.next().unwrap())),
+        },
+
+        //call_arg   = { name ~ "=" ~ expression }
+        Rule::call_arg => Ast::CallArg {
+            name: parse_name(inner.next().unwrap()),
+            expression: Box::new(parse_value_env(inner.next().unwrap())),
+        },
+
+        //call       = { name ~ "(" ~ call_arg ~ ("," ~ call_arg )* ~ ")" }
+        Rule::call => Ast::Call {
+            fn_name: parse_name(inner.next().unwrap()),
+            args: inner
+                .next()
+                .unwrap()
+                .into_inner()
+                .map(parse_value_env)
+                .collect(),
+        },
+
+        //submodel   = { "use" ~ call ~ ("as" ~ name)? }
+        Rule::submodel => {
+            // TODO: is there a better way of destructuring this?
+            let submodel_name: String = "".to_string();
+            let submodel_args: Vec<Ast> = Vec::new();
+            if let Ast::Call { fn_name, args } = parse_value_env(inner.next().unwrap()) {
+                submodel_name = fn_name;
+                submodel_args = args
+            } else {
+                unreachable!()
+            };
+            let local_name = parse_name(inner.next().unwrap());
+            Ast::Submodel {
+                name: submodel_name,
+                local_name,
+                args: submodel_args,
+            }
+        }
+
+        //rate_equation = { "dot" ~ "(" ~ name ~ ")" ~ "+=" ~ expression }
+        Rule::rate_equation => Ast::RateEquation {
+            name: parse_name(inner.next().unwrap()),
+            rhs: Box::new(parse_value_env(inner.next().unwrap())),
+        },
+
+        //equation   = { expression ~ "=" ~ expression }
+        Rule::equation => Ast::Equation {
+            lhs: Box::new(parse_value_env(inner.next().unwrap())),
+            rhs: Box::new(parse_value_env(inner.next().unwrap())),
+        },
+
+        //expression = { sign ~ term ~ (term_op ~ term)* }
+        Rule::expression => {
+            let sign = if inner.peek().unwrap().as_rule() == Rule::sign {
+                Some(parse_sign(inner.next().unwrap()))
+            } else {
+                None
+            };
+            let head_term = parse_value_env(inner.next().unwrap());
+            while inner.peek().is_some() {
+                //term_op    = @{ "-"|"+" }
+                let term_op = parse_sign(inner.next().unwrap());
+                let rhs_term = parse_value_env(inner.next().unwrap());
+                head_term = Ast::Binop {
+                    op: term_op,
+                    left: Box::new(head_term),
+                    right: Box::new(rhs_term),
+                };
+            }
+            if sign.is_some() {
+                Ast::Monop {
+                    op: sign.unwrap(),
+                    child: Box::new(head_term),
+                }
+            } else {
+                head_term
+            }
+        }
+
+        //term       = { factor ~ (factor_op ~ factor)* }
+        Rule::term => {
+            let head_factor = parse_value_env(inner.next().unwrap());
+            while inner.peek().is_some() {
+                let factor_op = parse_sign(inner.next().unwrap());
+                let rhs_factor = parse_value_env(inner.next().unwrap());
+                head_factor = Ast::Binop {
+                    op: factor_op,
+                    left: Box::new(head_factor),
+                    right: Box::new(rhs_factor),
+                };
+            }
+            head_factor
+        }
+        _ => unreachable!(),
     }
 }
 
-fn parse_string(text: &str) -> Result<Vec<Model>, Error<Rule>> {
+fn parse_string(text: &str) -> Result<Ast, Error<Rule>> {
     let main = MsParser::parse(Rule::main, &text)?.next().unwrap();
-    let models = main
-        .into_inner()
-        .filter(|m| m.as_rule() == Rule::model)
-        .map(|m| match m.as_rule() {
-            Rule::model => Model::create(m),
-            _ => unreachable!(),
-        });
-    return Ok(models.collect());
+    let env = Env {
+        models: Vec::new(),
+        vars: Vec::new(),
+        domains: Vec::new(),
+    };
+    let ast = parse_value(main, env);
+    return Ok(ast);
 }
 
 #[cfg(test)]

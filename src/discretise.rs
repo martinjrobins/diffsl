0f592dc0a4aabb1616603f26dd9e21a726ea9329
martinjrobins <martinjrobins@gmail.com>
2022-11-06 17:41:23 +0000

    add ds grammar

--- src/discretise.rs
+++ src/discretise.rs
@@ -1,7 +1,22 @@
 
+ 
 #[derive(Debug)]
+// F(t, u, u_dot) = G(t, u)
+pub struct Function {
+    pub args: Vec<Ast<'a>>,
+    pub stmts: Vec<Ast<'a>>,
+}
+ 
+
+
+#[derive(Debug)]
+// F(t, u, u_dot) = G(t, u)
 pub struct DiscreteModel<'a> {
-    pub eqns: Vec<Ast<'a>>,
+    // F(t, u, u_dot)
+    pub f_func: Function,
+    // G(t, u)
+    pub g_func: Function,
+    pub n_states: usize,
 }
 
 fn discretise<'s, 'a, 'mi>(model: &'a Ast<'s>, models: I, info: &'mi ModelInfo<'s, 'a>) -> DiscreteModel<'a> {

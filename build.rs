ac51d3f2bae3917684c223841280202b389510e9
martinjrobins <martinjrobins@gmail.com>
2024-12-11 13:34:52 +0000

    add include dir for macos

--- build.rs
+++ build.rs
@@ -26,14 +26,16 @@ mod enzyme {
             .clang_arg("-x")
             .clang_arg("c++");
 
-        //if mac:
-        //    macos_sdk_dir = env::var("MACOS_SDK_DIR").expect("MACOS_SDK_DIR not set");
-        //    builder = builder.clang_arg(format!("-isysroot {}", macos_sdk_dir));
-
+        
         // add include dirs
         for dir in inc_dirs {
             builder = builder.clang_arg(format!("-I{}", dir))
         }
+        if cfg!(target_os = "macos") {
+            let xcode_inc_dir = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include";
+            builder = builder.clang_arg(format!("-I{}", xcode_inc_dir));
+        }
+
         builder.generate()
     }
 

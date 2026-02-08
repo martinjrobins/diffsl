77d37ade2697d76b87bb0c7d5350248e4649bcd5
Martin Robinson <martinjrobins@gmail.com>
2026-02-08 15:37:12 +0000

    feat: can turn on one set of external functions at a time (#106)
    * feat: can turn on one set of external functions at a time
    
    * ci

--- diffsl/tests/external_f64.rs
+++ diffsl/tests/external_f64.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "external")]
+#[cfg(feature = "external_f64")]
 include!("support/external_test_macros.rs");
 
-#[cfg(feature = "external")]
+#[cfg(feature = "external_f64")]
 define_external_test!(f64, external_module_compiler_runs_f64);

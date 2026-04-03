a02d3cb742cb8163ea681ff47c96e6e7bc3b8756
martinjrobins <martinjrobins@users.noreply.github.com>
2026-04-03 10:40:26 +0000

    deploy: dae8f9182324a681c67e22c7709e6a790161dd2a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-87f2ae87.js',
+                path_to_root + 'searchindex-9216d466.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

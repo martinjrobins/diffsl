32e89f9b252b925d4219efe1711b48962364ddcf
Martin Robinson <martinjrobins@gmail.com>
2026-04-12 14:24:33 +0100

    fix: external_dynamic tests (#121)
    * fix: ci
    
    * cargo fmt
    
    * fix: windows

--- diffsl/src/execution/external_interface.rs
+++ diffsl/src/execution/external_interface.rs
@@ -45,6 +45,77 @@ macro_rules! for_each_external_symbol {
 #[allow(unused_imports)]
 pub(crate) use for_each_external_symbol;
 
+#[allow(unused_macros)]
+macro_rules! insert_external_symbols {
+    ($symbols:expr, $sym:path) => {{
+        use $sym as sym;
+        $symbols.insert("barrier_init".to_string(), sym::barrier_init as *const u8);
+        $symbols.insert("set_constants".to_string(), sym::set_constants as *const u8);
+        $symbols.insert("set_u0".to_string(), sym::set_u0 as *const u8);
+        $symbols.insert("reset".to_string(), sym::reset as *const u8);
+        $symbols.insert("reset_grad".to_string(), sym::reset_grad as *const u8);
+        $symbols.insert("reset_rgrad".to_string(), sym::reset_rgrad as *const u8);
+        $symbols.insert("reset_sgrad".to_string(), sym::reset_sgrad as *const u8);
+        $symbols.insert("reset_srgrad".to_string(), sym::reset_srgrad as *const u8);
+        $symbols.insert("rhs".to_string(), sym::rhs as *const u8);
+        $symbols.insert("rhs_grad".to_string(), sym::rhs_grad as *const u8);
+        $symbols.insert("rhs_rgrad".to_string(), sym::rhs_rgrad as *const u8);
+        $symbols.insert("rhs_sgrad".to_string(), sym::rhs_sgrad as *const u8);
+        $symbols.insert("rhs_srgrad".to_string(), sym::rhs_srgrad as *const u8);
+        $symbols.insert("mass".to_string(), sym::mass as *const u8);
+        $symbols.insert("mass_rgrad".to_string(), sym::mass_rgrad as *const u8);
+        $symbols.insert("set_u0_grad".to_string(), sym::set_u0_grad as *const u8);
+        $symbols.insert("set_u0_rgrad".to_string(), sym::set_u0_rgrad as *const u8);
+        $symbols.insert("set_u0_sgrad".to_string(), sym::set_u0_sgrad as *const u8);
+        $symbols.insert("calc_out".to_string(), sym::calc_out as *const u8);
+        $symbols.insert("calc_out_grad".to_string(), sym::calc_out_grad as *const u8);
+        $symbols.insert(
+            "calc_out_rgrad".to_string(),
+            sym::calc_out_rgrad as *const u8,
+        );
+        $symbols.insert(
+            "calc_out_sgrad".to_string(),
+            sym::calc_out_sgrad as *const u8,
+        );
+        $symbols.insert(
+            "calc_out_srgrad".to_string(),
+            sym::calc_out_srgrad as *const u8,
+        );
+        $symbols.insert("calc_stop".to_string(), sym::calc_stop as *const u8);
+        $symbols.insert(
+            "calc_stop_grad".to_string(),
+            sym::calc_stop_grad as *const u8,
+        );
+        $symbols.insert(
+            "calc_stop_rgrad".to_string(),
+            sym::calc_stop_rgrad as *const u8,
+        );
+        $symbols.insert(
+            "calc_stop_sgrad".to_string(),
+            sym::calc_stop_sgrad as *const u8,
+        );
+        $symbols.insert(
+            "calc_stop_srgrad".to_string(),
+            sym::calc_stop_srgrad as *const u8,
+        );
+        $symbols.insert("set_id".to_string(), sym::set_id as *const u8);
+        $symbols.insert("get_dims".to_string(), sym::get_dims as *const u8);
+        $symbols.insert("set_inputs".to_string(), sym::set_inputs as *const u8);
+        $symbols.insert("get_inputs".to_string(), sym::get_inputs as *const u8);
+        $symbols.insert(
+            "set_inputs_grad".to_string(),
+            sym::set_inputs_grad as *const u8,
+        );
+        $symbols.insert(
+            "set_inputs_rgrad".to_string(),
+            sym::set_inputs_rgrad as *const u8,
+        );
+    }};
+}
+
+#[allow(unused_imports)]
+pub(crate) use insert_external_symbols;
+
 macro_rules! collect_external_symbol_names {
     ($($name:literal => $func:ident,)+) => {
         pub(crate) const EXTERNAL_SYMBOL_NAMES: &[&str] = &[$($name),+];
